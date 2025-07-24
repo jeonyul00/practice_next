addad38a41a8748cb251b8679dd5247fdbee3397
jeonyul <jeonyul00@gmail.com>
2025-07-24 17:52:38 +0900

    set: drizzle orm

---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+import { pgTable, integer, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  clerkId: varchar({ length: 255 }).notNull().unique(),
+  name: varchar({ length: 255 }).notNull().unique(),
+  imageUrl: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
