5fcb44ac4636450cff7aaa8cf83f969ef2865ffb
jeonyul <jeonyul00@gmail.com>
2025-08-15 20:48:17 +0900

    update

--- page-router/src/pages/search/index.tsx
+++ page-router/src/pages/search/index.tsx
@@ -1,13 +1,30 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from "next";
+import fetchBooks from "@/lib/fetch-books";
+import { BookData } from "@/types";
 
-export default function Page() {
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const q = context.query.q;
+  const books = await fetchBooks(q as string);
+
+  return { props: { books } };
+};
+export default function Page({
+  books,
+}: {
+  books: InferGetServerSidePropsType<GetServerSideProps>;
+}) {
   return (
     <div>
-      {books.map((book) => (
+      {books.map((book: BookData) => (
         <BookItem key={book.id} {...book} />
       ))}
     </div>
